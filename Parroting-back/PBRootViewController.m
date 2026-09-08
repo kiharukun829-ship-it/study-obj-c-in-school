451956e4a107e8b532850100b8df8d9c905000c0
kiharukun829-ship-it <kiharukun829@gmail.com>
2026-09-08 14:50:58 +0900

    Update PBRootViewController.m

--- Parroting-back/PBRootViewController.m
+++ Parroting-back/PBRootViewController.m
@@ -1,22 +1,41 @@
 #import "PBRootViewController.h"
 
-@implementation PBRootViewController
-
+@interface PBRootViewController ()
+@property (nonatomic, strong) UITextField *Textget;
+@end
 - (void)viewDidLoad {
     [super viewDidLoad];
-    self.title = @"Parroting-back";
-    
-    // iOS 6スタイルの背景色
-    self.view.backgroundColor = [UIColor whiteColor];
+    [self UIs];
 }
-
-// iOS 6以降の回転制御方法
+- (void)UIs {
+    self.Textget = [[UITextField alloc] initWithFrame:CGRectMake(20,100,80,40)];
+    self.Textget.borderStyl = UITextBorderStyleBezel;
+    self.Textget.placeholder = @"input text...";
+    self.Textget.keyboardType = UIKeyboardTypeDefault;
+    [self.view addSubview:self.Textget];
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+    button.frame = CGRectMake(150, 100, 280, 50);
+    [button setTitle:@"Plese Tap" forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(buttonTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+}
+- (void)buttonTapped:(button *)sender {
+        NSStoring *hasaba = self.Textget.text
+        //アラート処理
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"result"
+                                                  message:@"🐦:hasaba"
+                                                  delegate:self
+                                                  cancelButtonTitle:@"OK"
+                                                  otherButtonTitles:nil];
+        [alert show]
+}
+// iOS 6以降の回転制御方法（回転を許可しない）
 - (BOOL)shouldAutorotate {
-    return YES;
+    return NO;
 }
 
 - (NSUInteger)supportedInterfaceOrientations {
-    return UIInterfaceOrientationMaskAllButUpsideDown;
+    return UIInterfaceOrientationMaskPortrait;
 }
 
 @end
